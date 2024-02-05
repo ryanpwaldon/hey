88a8351b41a8b60499bddd851f639b4d2cebe973
Ryan Waldon <ryanpwaldon@gmail.com>
2024-02-05 12:29:53 +1100

    feat: publication attatchment menu headlessui → radixui

--- apps/web/src/components/Composer/Actions/Attachment.tsx
+++ apps/web/src/components/Composer/Actions/Attachment.tsx
@@ -1,24 +1,20 @@
 import type { ChangeEvent, FC } from 'react';
 
-import MenuTransition from '@components/Shared/MenuTransition';
-import { Menu } from '@headlessui/react';
 import {
   MusicalNoteIcon,
   PhotoIcon,
   VideoCameraIcon
 } from '@heroicons/react/24/outline';
-import { Spinner, Tooltip } from '@hey/ui';
-import cn from '@hey/ui/cn';
+import { Menu, Spinner, Tooltip } from '@hey/ui';
 import {
   MediaAudioMimeType,
   MediaImageMimeType
 } from '@lens-protocol/metadata';
 import { motion } from 'framer-motion';
-import { useId, useRef, useState } from 'react';
+import { useId, useState } from 'react';
 import toast from 'react-hot-toast';
 import useUploadAttachments from 'src/hooks/useUploadAttachments';
 import { usePublicationAttachmentStore } from 'src/store/non-persisted/publication/usePublicationAttachmentStore';
-import { useOnClickOutside } from 'usehooks-ts';
 
 const ImageMimeType = Object.values(MediaImageMimeType);
 const AudioMimeType = Object.values(MediaAudioMimeType);
@@ -40,9 +36,6 @@ const Attachment: FC = () => {
   const { handleUploadAttachments } = useUploadAttachments();
   const [showMenu, setShowMenu] = useState(false);
   const id = useId();
-  const dropdownRef = useRef(null);
-
-  useOnClickOutside(dropdownRef, () => setShowMenu(false));
 
   const isTypeAllowed = (files: FileList) => {
     const allowedTypes = [
@@ -96,98 +89,71 @@ const Attachment: FC = () => {
   };
 
   return (
-    <Menu as="div">
-      <Menu.Button
-        aria-label="More"
-        as={motion.button}
-        className="outline-brand-500 rounded-full outline-offset-8"
-        onClick={() => setShowMenu(!showMenu)}
-        whileTap={{ scale: 0.9 }}
-      >
-        {isUploading ? (
-          <Spinner size="sm" />
-        ) : (
-          <Tooltip content="Media" placement="top">
-            <PhotoIcon className="text-brand-500 size-5" />
-          </Tooltip>
-        )}
-      </Menu.Button>
-      <MenuTransition show={showMenu}>
-        <Menu.Items
-          className="absolute z-[5] mt-2 rounded-xl border bg-white py-1 shadow-sm focus:outline-none dark:border-gray-700 dark:bg-gray-900"
-          ref={dropdownRef}
-          static
-        >
-          <Menu.Item
-            as="label"
-            className={({ active }) =>
-              cn(
-                { 'dropdown-active': active },
-                'menu-item !flex cursor-pointer items-center gap-1 space-x-1 rounded-lg'
-              )
-            }
-            disabled={disableImageUpload()}
-            htmlFor={`image_${id}`}
+    <div>
+      <Menu.Root open={showMenu}>
+        <Menu.Trigger>
+          <motion.button
+            aria-label="More"
+            className="outline-brand-500 rounded-full outline-offset-8"
+            onClick={() => setShowMenu(!showMenu)}
+            whileTap={{ scale: 0.9 }}
           >
-            <PhotoIcon className="text-brand-500 size-4" />
-            <span className="text-sm">Upload image(s)</span>
-            <input
-              accept={ImageMimeType.join(',')}
-              className="hidden"
-              disabled={disableImageUpload()}
-              id={`image_${id}`}
-              multiple
-              onChange={handleAttachment}
-              type="file"
-            />
+            {isUploading ? (
+              <Spinner size="sm" />
+            ) : (
+              <Tooltip content="Media" placement="top">
+                <PhotoIcon className="text-brand-500 size-5" />
+              </Tooltip>
+            )}
+          </motion.button>
+        </Menu.Trigger>
+        <Menu.Content onInteractOutside={() => setShowMenu(false)}>
+          <Menu.Item asChild disabled={disableImageUpload()}>
+            <label className="flex items-center gap-2" htmlFor={`image_${id}`}>
+              <PhotoIcon className="text-brand-500 size-4" />
+              <span className="text-sm">Upload image(s)</span>
+              <input
+                accept={ImageMimeType.join(',')}
+                className="hidden"
+                disabled={disableImageUpload()}
+                id={`image_${id}`}
+                multiple
+                onChange={handleAttachment}
+                type="file"
+              />
+            </label>
           </Menu.Item>
-          <Menu.Item
-            as="label"
-            className={({ active }) =>
-              cn(
-                { 'dropdown-active': active },
-                'menu-item !flex cursor-pointer items-center gap-1 space-x-1 rounded-lg'
-              )
-            }
-            disabled={Boolean(attachments.length)}
-            htmlFor={`video_${id}`}
-          >
-            <VideoCameraIcon className="text-brand-500 size-4" />
-            <span className="text-sm">Upload video</span>
-            <input
-              accept={VideoMimeType.join(',')}
-              className="hidden"
-              disabled={Boolean(attachments.length)}
-              id={`video_${id}`}
-              onChange={handleAttachment}
-              type="file"
-            />
+          <Menu.Item asChild disabled={Boolean(attachments.length)}>
+            <label className="flex items-center gap-2" htmlFor={`video_${id}`}>
+              <VideoCameraIcon className="text-brand-500 size-4" />
+              <span className="text-sm">Upload video</span>
+              <input
+                accept={VideoMimeType.join(',')}
+                className="hidden"
+                disabled={Boolean(attachments.length)}
+                id={`video_${id}`}
+                onChange={handleAttachment}
+                type="file"
+              />
+            </label>
           </Menu.Item>
-          <Menu.Item
-            as="label"
-            className={({ active }) =>
-              cn(
-                { 'dropdown-active': active },
-                'menu-item !flex cursor-pointer items-center gap-1 space-x-1 rounded-lg'
-              )
-            }
-            disabled={Boolean(attachments.length)}
-            htmlFor={`audio_${id}`}
-          >
-            <MusicalNoteIcon className="text-brand-500 size-4" />
-            <span className="text-sm">Upload audio</span>
-            <input
-              accept={AudioMimeType.join(',')}
-              className="hidden"
-              disabled={Boolean(attachments.length)}
-              id={`audio_${id}`}
-              onChange={handleAttachment}
-              type="file"
-            />
+          <Menu.Item asChild disabled={Boolean(attachments.length)}>
+            <label className="flex items-center gap-2" htmlFor={`audio_${id}`}>
+              <MusicalNoteIcon className="text-brand-500 size-4" />
+              <span className="text-sm">Upload audio</span>
+              <input
+                accept={AudioMimeType.join(',')}
+                className="hidden"
+                disabled={Boolean(attachments.length)}
+                id={`audio_${id}`}
+                onChange={handleAttachment}
+                type="file"
+              />
+            </label>
           </Menu.Item>
-        </Menu.Items>
-      </MenuTransition>
-    </Menu>
+        </Menu.Content>
+      </Menu.Root>
+    </div>
   );
 };
 
