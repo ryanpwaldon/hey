014f80761e07a8223b16c9e0cd3346800f71b30d
Ryan Waldon <ryanpwaldon@gmail.com>
2024-02-05 11:58:20 +1100

    feat: set default menu alignment

--- packages/ui/src/Menu.tsx
+++ packages/ui/src/Menu.tsx
@@ -62,6 +62,7 @@ const MenuContent = forwardRef<
 >(({ className, sideOffset = 4, ...props }, ref) => (
   <MenuPrimitive.Portal>
     <MenuPrimitive.Content
+      align="start"
       className={cn(
         'z-50 min-w-[8rem] space-y-2 overflow-hidden rounded-xl border bg-white p-2 dark:border-gray-700 dark:bg-gray-900',
         'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
